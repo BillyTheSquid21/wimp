d4564c36c5d7204b86d3862c88f6828c46b55c0e
Will Martin <97798337+BillyTheSquid21@users.noreply.github.com>
2024-09-27 18:51:36 +0100

    initial

---
+++ external/CS/tests/1_SEND_RECIEVE_LOOP/WIMP-CS-Test-01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using WimpCS;
+
+namespace WIMP_CS_Test_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wimp.wimp_init();
+            Thread.Sleep(10000);
+            Wimp.wimp_shutdown();
+        }
+    }
+}
