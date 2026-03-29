b89e4dc07f3be605287ce166eb15c3e4c75b3895
Will Martin <willmartin201@gmail.com>
2026-03-29 18:18:01 +0100

    trigger jobs

--- tests/utility/wimp-test/wimp-test.py
+++ tests/utility/wimp-test/wimp-test.py
@@ -119,7 +119,7 @@ def main():
             report["results"]["summary"]["skipped"] += 1
             continue
 
-        result = subprocess.run([base_test], capture_output=True, text=True, cwd=executable_root)
+        result = subprocess.run([os.path.basename(base_test)], capture_output=True, text=True, cwd=executable_root)
         output_lines = result.stdout.splitlines()
 
         # Parse the output and check for success
